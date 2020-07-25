2eb81586f64696c3ac9fd8fd261d8f6edd6a2ad0
DadongZ <ddzhangzz@gmail.com>
2020-07-25 17:30:02 -0400

    Updates

---
+++ precache-manifest.dedb03172933eed231fd5ae36e45bd37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de414f500ef403c6e1c6daef9a273d18",
+    "url": "/manongjobs/index.html"
+  },
+  {
+    "revision": "525ac43e84c0fe1f48ca",
+    "url": "/manongjobs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a8ca31d056d94a8f442e",
+    "url": "/manongjobs/static/js/2.f20ea4cc.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/manongjobs/static/js/2.f20ea4cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525ac43e84c0fe1f48ca",
+    "url": "/manongjobs/static/js/main.fc01109d.chunk.js"
+  },
+  {
+    "revision": "9a7ac41731669a0c9bbf",
+    "url": "/manongjobs/static/js/runtime-main.834e4d0b.js"
+  }
+]);
